41829021beedc96815b1dbfea5bbc9ea7f8f684b
fanglang <m17621995364@163.com>
2020-02-26 18:21:11 +0800

    增加一行代码

--- GitDemo/Program.cs
+++ GitDemo/Program.cs
@@ -10,6 +10,7 @@ namespace GitDemo
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("My Name is fanglang");
             Console.WriteLine("...");
         }
     }
